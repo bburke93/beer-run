b2a5f57e71fd6680bd4eb333ed4a08692cca2b18
bburke93 <brandonburke93@outlook.com>
2017-04-05 22:35:49 -0600

    Initial commit of final project

---
+++ src/components/Head/Head.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Head.css';
+
+class Head extends Component{
+  render(){
+    return(
+      <div className="container">
+        <header className="head">
+          <h1>&#127866; Beer Run</h1>
+          <nav className="menu">
+            <a href="#" className="menu-toggle">
+              <span className="trigger">&#9776;</span>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+              </ul>
+            </a>
+          </nav>
+        </header>
+      </div>
+    )
+  } 
+}
+
+export default Head;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './components/App';
+import Head from './components/Head';
+import './index.css';
+
+ReactDOM.render(
+  <Head />,
+  document.getElementById('root')
+);
