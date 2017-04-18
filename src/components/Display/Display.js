9e56d7b92c27cc893f21832d32c5995e61c74125
bburke93 <brandonburke93@outlook.com>
2017-04-17 22:07:52 -0600

    Adjust display of results

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default ({
   name = "Name missing",
-  category, address
+  category, address, cityState
 }) => (
   <div className="main">
     <div className="constrain">
       <h2>{name}</h2>
-      <h3>{category}</h3>
-      <h3>{address}</h3>
+      <h4>{category}</h4>
+      <h3>{address}<br/>{cityState}</h3>
     </div>
   </div>
 );
--- src/components/FindBeer/FindBeer.js
+++ src/components/FindBeer/FindBeer.js
@@ -7,29 +7,38 @@ class FindBeer extends Component{
   constructor(props){
     super(props);
     this.action = this.action.bind(this);
-    const defaultState = { locs: [] };
+    const defaultState = { locs: [], isNull: false };
     this.storageKey = 'beerLocations';
-    this.state = JSON.parse(localStorage.getItem(this.storageKey)) || defaultState;
+    this.state = defaultState;
+    //this.state = JSON.parse(localStorage.getItem(this.storageKey)) || defaultState;
   }
 
   action({value}){
     beerMap(value)
       .then(res => {
         if(res[0].id == null){
+          this.setState(state => ({
+            locs: [...state.locs],
+            isNull: true
+          }));
           return Promise.reject();
         }
+        return res;
       })
       .then(res => res[0])
       .then(res => ({
         name: res.name,
         category: res.status,
-        address: res.street
+        address: res.street,
+        cityState: res.city + ', ' + res.state
       }))
       .then(res => {
         this.setState(state => ({
-          locs: [res, ...state.locs]
+          locs: [res],
+          //locs: [res, ...state.locs],
+          isNull: false
         }));
-        localStorage.setItem(this.storageKey, JSON.stringify(this.state))
+        sessionStorage.setItem(this.storageKey, JSON.stringify(this.state));
       })
   }
 
@@ -40,7 +49,7 @@ class FindBeer extends Component{
           <h1>Search by city and state.</h1>
           <Form submitAction={this.action} />
           {
-            this.state.locs.length ?  
+            this.state.isNull === false ?  
             this.state.locs.map((loc,key) => <Display {...loc} key={key} />):
             <div>No results found</div>
           }
